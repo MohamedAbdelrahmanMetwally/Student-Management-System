5fd54127464c77d430931613a856ba9440084eee
mohamed <mnafe44@gmail.com>
2025-11-19 09:26:42 +0200

    fininshing main

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'student.dart';
+import 'student_manager.dart';
+
+void main(List<String> args) {
+  StudentManager studentManager = StudentManager();
+  while (true) {
+    print("a-add a student");
+    print("b-remove a student");
+    print("c-update a student");
+    print("d-show all students");
+    print("e-search for a student");
+    print("f-exit");
+    stdout.write("Enter your choice : ");
+    String answer = stdin.readLineSync()!;
+    if (answer == "a") {
+      stdout.write("Enter the id : ");
+      String id = stdin.readLineSync()!;
+      stdout.write("Enter the name : ");
+      String name = stdin.readLineSync()!;
+      stdout.write("Enter the gpa : ");
+      double gpa = double.parse(stdin.readLineSync()!);
+      studentManager.addStudent(new Student(gpa, id, name));
+      print("added succesfully");
+    } else if (answer == "b") {
+      stdout.write("Enter the id : ");
+      String id = stdin.readLineSync()!;
+      studentManager.removeStudentById(id);
+      print("removed succesfully");
+    } else if (answer == 'c') {
+      stdout.write("Enter the id : ");
+      String id = stdin.readLineSync()!;
+      stdout.write("Enter the name : ");
+      String name = stdin.readLineSync()!;
+      stdout.write("Enter the gpa : ");
+      double gpa = double.parse(stdin.readLineSync()!);
+      studentManager.updateStudent(new Student(gpa, id, name));
+      print("updated succesfully succesfully");
+    } else if (answer == "d") {
+      studentManager.showAllStuden();
+    } else if (answer == "e") {
+      stdout.write("Enter the id : ");
+      String id = stdin.readLineSync()!;
+      var student = studentManager.getStudentById(id);
+      if (student == null) {
+        print("Student not found.\n");
+      } else {
+        print(student);
+        print("Search complete.\n");
+      }
+    } else {
+      print("Thanks for your time");
+      break;
+    }
+  }
+}
